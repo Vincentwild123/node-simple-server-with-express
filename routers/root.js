e3cffc0d7e774ebb54fea6e8f67c1f856b9d3ae3
vincent <18wjchen1@stu.edu.cn>
2021-04-27 09:32:34 +0800

    BUG Fixed

--- routers/root.js
+++ routers/root.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.end('index page');
+  res.render('index');
 });
 
 module.exports = router;
--- routers/user.js
+++ routers/user.js
@@ -5,7 +5,7 @@ router.get('/', function (req, res, next) {
   res.end('get user');
 });
 
-router.post('/', function (req, res, next) => {
+router.post('/', function (req, res, next) {
   res.end('post user')
 })
 
